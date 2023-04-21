f958c90fb6081e431f576be6eb7ab76119a9e9f9
Mike <107565159+cryptotuts@users.noreply.github.com>
2023-04-21 19:02:49 +0200

    Update 500gb0904.sh

--- 500gb0904.sh
+++ 500gb0904.sh
@@ -10,49 +10,19 @@ function logo {
   curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
 }
 
-function line_1 {
+function line {
   echo -e "${GREEN}-----------------------------------------------------------------------------${NORMAL}"
 }
 
-function line_2 {
-  echo -e "${RED}##############################################################################${NORMAL}"
-}
-
-function install_tools {
-  sudo apt update && sudo apt install mc wget htop jq git -y
-}
-
-function install_docker {
-  curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh | bash
-}
-
-function install_ufw {
-  curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash
-}
-
-function read_nodename {
-  if [ ! $SUBSPACE_NODENAME ]; then
-  echo -e "Enter your node name(random name for telemetry)"
-  line_1
-  read SUBSPACE_NODENAME
-  fi
-}
-
-function read_wallet {
-  if [ ! $WALLET_ADDRESS ]; then
-  echo -e "Enter your polkadot.js extension address"
-  line_1
-  read WALLET_ADDRESS
-  fi
-}
-
 function get_vars {
-  export CHAIN="gemini-3c"
-  export RELEASE="gemini-3c-2023-mar-23"
+  export CHAIN="gemini-3d"
+  export RELEASE="gemini-3d-2023-apr-18"
+  export SUBSPACE_NODENAME=$(cat $HOME/subspace_docker/docker-compose.yml | grep "\-\-name" | awk -F\" '{print $4}')
+  export WALLET_ADDRESS=$(cat $HOME/subspace_docker/docker-compose.yml | grep "\-\-reward-address" | awk -F\" '{print $4}')
+  export PLOT_SIZE=$(cat $HOME/subspace_docker/docker-compose.yml | grep "\-\-plot-size" | awk -F\" '{print $4}')
 }
 
 function eof_docker_compose {
-  mkdir -p $HOME/subspace_docker/
   sudo tee <<EOF >/dev/null $HOME/subspace_docker/docker-compose.yml
   version: "3.7"
   services:
@@ -112,53 +82,54 @@ function eof_docker_compose {
 EOF
 }
 
-function docker_compose_up {
-  docker-compose -f $HOME/subspace_docker/docker-compose.yml up -d
+function check_fork {
+  sleep 30
+  check_fork=`docker logs --tail 100  subspace_docker_node_1 2>&1 | grep "Node is running on non-canonical fork"`
+  if [ -z "$check_fork" ]
+  then
+    echo -e "${GREEN}Нода не в форке - все ок${NORMAL}"
+  else
+    echo -e "${RED}Нода была в форке, выполняем сброс и перезапускаем${NORMAL}"
+    cd $HOME/subspace_docker/
+    docker-compose down
+    docker volume rm subspace_docker_farmer-data subspace_docker_node-data subspace_docker_subspace-farmer subspace_docker_subspace-node
+    docker-compose up -d
+  fi
 }
 
-function echo_info {
-  echo -e "${GREEN}Для остановки ноды и фармера subspace: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml down \n ${NORMAL}"
-  echo -e "${GREEN}Для запуска ноды и фармера subspace: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml up -d \n ${NORMAL}"
-  echo -e "${GREEN}Для перезагрузки ноды subspace: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml restart node \n ${NORMAL}"
-  echo -e "${GREEN}Для перезагрузки фармера subspace: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml restart farmer \n ${NORMAL}"
-  echo -e "${GREEN}Для проверки логов ноды выполняем команду: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml logs -f --tail=100 node \n ${NORMAL}"
-  echo -e "${GREEN}Для проверки логов фармера выполняем команду: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml logs -f --tail=100 farmer \n ${NORMAL}"
+function check_verif {
+  sleep 30
+  check_verif=`docker logs --tail 100  subspace_docker_node_1 2>&1 | grep "Verification failed for block"`
+  if [ -z "$check_verif" ]
+  then
+    echo -e "${GREEN}Ошибок верификации нет - все ок${NORMAL}"
+  else
+    echo -e "${RED}Есть ошибки верификации блоков, выполняем сброс и перезапускаем${NORMAL}"
+    cd $HOME/subspace_docker/
+    docker-compose down
+    docker volume rm subspace_docker_farmer-data subspace_docker_node-data subspace_docker_subspace-farmer subspace_docker_subspace-node
+    docker-compose up -d
+  fi
 }
 
-function delete_old {
-  docker-compose -f $HOME/subspace_docker/docker-compose.yml down -v &>/dev/null
-  docker volume rm subspace_docker_subspace-farmer subspace_docker_subspace-node &>/dev/null
+function update_subspace {
+  cd $HOME/subspace_docker/
+  docker-compose down -v
+  eof_docker_compose
+  docker-compose pull
+  docker-compose up -d
 }
 
 colors
-line_1
+line
 logo
-line_2
-read_nodename
-line_2
-read_wallet
-line_2
-echo -e "Установка tools, ufw, docker"
-line_1
-install_tools
-install_ufw
-install_docker
+line
 get_vars
-delete_old
-line_1
-echo -e "Создаем docker-compose файл"
-line_1
-eof_docker_compose
-line_1
-echo -e "Запускаем docker контейнеры для node and farmer Subspace"
-line_1
-docker_compose_up
-line_2
-echo_info
-line_2
+update_subspace
+line
+check_fork
+line
+# check_verif
+# line
+echo -e "${GREEN}=== Обновление завершено ===${NORMAL}"
+cd $HOME
